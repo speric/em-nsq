ae57879bd21ef32df37c4f37942e16ecf18f96eb
speric <eric@prudentiadigital.com>
2013-04-22 20:06:46 -0400

    Reorganized directory structure.

--- publisher_test.rb
+++
@@ -1,8 +0,0 @@
-require Dir.pwd + '/lib/em-nsq'
-
-EM.synchrony do
-  publisher = EMNSQ::Publisher.new(:host => "localhost", :port => "4150")
-  publisher.publish(:topic => "em-ruby-nsq-test-speric", :message => "testing NOW!")
-  publisher.finish
-  EM.stop
-end
--- reader_test.rb
+++
@@ -1,7 +0,0 @@
-require Dir.pwd + '/lib/em-nsq'
-
-EM.synchrony do
-  reader = EMNSQ::Reader.new(:host => "localhost", :port => "4150", :topic => "em-ruby-nsq-test-speric", :channel => "em-nsq-evented-test2") do |msg|
-    puts ">>: #{msg}"
-  end
-end
