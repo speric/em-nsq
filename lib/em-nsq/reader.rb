768c81b99c563a406feae73a4cfd6a8b20677cd2
speric <eric@prudentiadigital.com>
2013-04-22 20:15:17 -0400

    Merge cleanup.

--- lib/em-nsq/reader.rb
+++ lib/em-nsq/reader.rb
@@ -2,14 +2,9 @@ module EMNSQ
   class Reader < EMNSQ::Base
     attr_accessor :max_in_flight
     
-<<<<<<< HEAD
     def initialize(*args)
       super
-=======
-    def initialize(*args, &block)
-      super
       @call_back     = block
->>>>>>> d9ed9149faa515319e3b58d262a74c17a1da23d7
       @topic         = @options[:topic]
       @channel       = @options[:channel]
       @max_in_flight = @options[:max_in_flight] ||= 2500
@@ -17,14 +12,11 @@ module EMNSQ
       @short_id      = @long_id.split('.')[0]
       
       subscribe
-<<<<<<< HEAD
     end
     
     def message(&block)
       @call_back = block
-=======
       run
->>>>>>> d9ed9149faa515319e3b58d262a74c17a1da23d7
     end
     
     def run
@@ -38,10 +30,7 @@ module EMNSQ
     
     def subscribe
       @socket.write("SUB #{@topic} #{@channel} #{@short_id} #{@long_id}\n")
-<<<<<<< HEAD
       @socket.write("RDY #{@max_in_flight}\n")
-=======
->>>>>>> d9ed9149faa515319e3b58d262a74c17a1da23d7
     end
   end
 end
